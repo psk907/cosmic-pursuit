ea9bbe83cd13a732f8063aa67031775ceeee3bd6
Aakash <p.saiaakash518@gmail.com>
2022-06-05 13:30:03 +0530

    added error log

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 var mongoClient = require("mongodb").MongoClient;
 mongoClient.connect(process.env.MONGO_CONNECTION_URL, function (err, client) {
+  console.log(err)
   console.log("Connected successfully to database");
   let db = client.db('lostinspace');
   require("./server")(db);
